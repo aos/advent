05ecb05ff44291318997637c03f03eef2e689337
Aos <aosdab@gmail.com>
2018-12-03 18:26:29 -0500

    Cleaned up code

--- day03/01.py
+++ day03/01.py
@@ -4,8 +4,23 @@
 import re
 
 
-w, h = 1000, 1000
-FABRIC = [[0 for x in range(w)] for y in range(h)]
+def build_fabric(w, h):
+    return [[0 for x in range(w)] for y in range(h)]
+
+
+def populate(query, fabric):
+    claim_id, start_point, dimensions = split_query(query)
+
+    x, y = [int(i) for i in start_point.split(',')]
+    width, height = [int(i) for i in dimensions.split('x')]
+
+    for i in range(y, y+height):
+        for j in range(x, x+width):
+            fabric[i][j] += 1
+
+
+def split_query(q):
+    return re.match(r'^(#\d+)\D+(\d+,\d+)\D+(\d+x\d+)$', q).groups()
 
 
 def intersecting(fabric):
@@ -18,31 +33,24 @@ def intersecting(fabric):
     return claims
 
 
-def populate(query, fabric):
-    claim_id, start_point, dimensions = \
-        re.match(r'^(#\d+)\D+(\d+,\d+)\D+(\d+x\d+)$', query).groups()
-
-    x, y = [int(i) for i in start_point.split(',')]
-    width, height = [int(i) for i in dimensions.split('x')]
-
-    for i in range(y, y+height):
-        for j in range(x, x+width):
-            fabric[i][j] += 1
-
-
 with open('./day03-input.txt') as f:
+    FABRIC = build_fabric(1000, 1000)
+
     for line in f:
         populate(line, FABRIC)
 
     res = intersecting(FABRIC)
     print(res)
 
+# Tests
 test_input = [
     '#1 @ 1,3: 4x4',
     '#2 @ 3,1: 4x4',
     '#3 @ 5,5: 2x2'
 ]
-TEST_FABRIC = [[0 for x in range(8)] for y in range(8)]
+TEST_FABRIC = build_fabric(8, 8)
 for i in test_input:
     populate(i, TEST_FABRIC)
-intersecting(TEST_FABRIC)
+assert(split_query(test_input[0]) == ('#1', '1,3', '4x4'))
+assert(intersecting(TEST_FABRIC) == 4)
+print('All tests passed!')
