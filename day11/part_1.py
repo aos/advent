fcc8baf7e62504b9e77eb2776bddd659de090fe6
Aos <aosdab@gmail.com>
2018-12-11 08:41:46 -0500

    Started day11

---
+++ day11/part_1.py
@@ -0,0 +1,10 @@
+# Day 11 - Puzzle 1
+# What is the <X,Y> coordinate of the top-left fuel cell of the 3x3 square with
+# the largest total power?
+
+
+def charge(sn):
+    pass
+
+
+PUZZLE_INPUT = 2187
