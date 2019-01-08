cbcbcaeee77a4e423a18e48a73528b1f5a90c0c5
Aos <aosdab@gmail.com>
2019-01-08 00:54:03 -0500

    Finished parsing input for day24 -- onto the logic

--- day24/part_1.py
+++ day24/part_1.py
@@ -20,9 +20,12 @@
 # Repeat combat until 1 army remaining
 
 
+import re
+
+
 class Group:
-    def __init__(self, army, units, hp, att_dmg, att_type, init, weak, imms):
-        self.army = army
+    def __init__(self, a_type, units, hp, att_dmg, att_type, init, weak, imms):
+        self.a_type = a_type
         self.num_units = units
         self.hp = hp
         self.att_dmg = att_dmg
@@ -32,3 +35,54 @@ class Group:
         self.imms = imms
         self.ep = units * att_dmg
         self.alive = True
+
+    def __repr__(self):
+        return ('Group({}, Units: {}, HP: {}, DMG: {}, D_type: {}, Init: {}, '
+                'Weakness: {}, Immune: {}, EP: {}, {})').format(
+            self.a_type, self.num_units, self.hp, self.att_dmg, self.att_type,
+            self.init, self.weaks, self.imms, self.ep, self.alive
+        )
+
+    def __str__(self):
+        return f'Group({self.a_type}, {self.num_units})'
+
+
+def parse_input(file):
+    a_type = None
+    groups = []
+    with open(file) as f:
+        for line in f:
+            line = line.strip()
+            if not line:
+                continue
+
+            if 'Immune' in line:
+                a_type = 0
+                continue
+            if 'Infection' in line:
+                a_type = 1
+                continue
+
+            num_units, hp, dmg, init = list(map(int, re.findall(r'\d+', line)))
+            weaks = re.search(r'(?<=weak to ).[^;\)]*', line) or []
+            imms = re.search(r'(?<=immune to ).[^;\)]*', line) or []
+            att_type = re.search(r'(\w+)(?= damage)', line).group(0)
+
+            if weaks:
+                weaks = [x.strip() for x in re.search(
+                    r'(?<=weak to ).[^;\)]*', line
+                ).group(0).split(',')]
+            if imms:
+                imms = [x.strip() for x in re.search(
+                    r'(?<=immune to ).[^;\)]*', line
+                ).group(0).split(',')]
+
+            groups.append(
+                Group(a_type, num_units, hp, dmg, att_type, init, weaks, imms)
+            )
+
+    return groups
+
+
+# Tests
+p = parse_input('./example-input.txt')
