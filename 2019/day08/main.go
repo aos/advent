360a602ad4fb97b7d68789a8a3ca405be0424c39
Aos <aosdab@gmail.com>
2019-12-23 02:22:23 -0500

    Finished day 8 part 2

--- 2019/day08/main.go
+++ 2019/day08/main.go
@@ -8,17 +8,18 @@ import (
 )
 
 func main() {
-	var image [][][]byte
-	width := 2
-	height := 2
-	layerSize := width * height
-	nums := parseFile("./day08-example2.txt")
+	width := 25
+	height := 6
+	nums := parseFile("./day08-input.txt")
 
+	var image [][][]byte
 	var layer [][]byte
 	var row []byte
-	lastZeroesCount := math.MaxInt32
+	// Part 1 stuff
+	lastZeroesCount := math.MaxInt16
 	numZeroes := 0
 	zeroesLayer := 0
+
 	for i, n := range nums {
 		if n == '0' {
 			numZeroes++
@@ -30,7 +31,7 @@ func main() {
 			row = nil
 		}
 
-		if (i+1)%layerSize == 0 {
+		if (i+1)%(width*height) == 0 {
 			image = append(image, layer)
 			layer = nil
 
@@ -42,11 +43,11 @@ func main() {
 			numZeroes = 0
 		}
 	}
-	// fmt.Println(multiplyDigits(image[zeroesLayer]))
+	fmt.Println("Part 1:", multiplyDigits(image[zeroesLayer]))
 
-	fmt.Println(zeroesLayer)
-	// fmt.Println(image)
-	renderImage(image)
+	fmt.Println("Part 2:")
+	img := createImage(image, width, height)
+	renderImage(img)
 }
 
 // Part 1
@@ -69,19 +70,39 @@ func multiplyDigits(layer [][]byte) int {
 }
 
 // Part 2
-func renderImage(image [][][]byte) {
-	width := 2
-	height := 2
+func createImage(image [][][]byte, width, height int) [][]byte {
 	numLayers := len(image)
-	// var final [][]byte
+	final := make([][]byte, height)
+	for i := range final {
+		final[i] = make([]byte, width)
+	}
 
-	for i := 0; i < width; i++ {
-		for j := 0; j < height; j++ {
-			var row []byte
+	for w := 0; w < width; w++ {
+		for h := 0; h < height; h++ {
 			for l := 0; l < numLayers; l++ {
-				row = append(row, image[l][j][i])
+				if image[l][h][w] == '2' {
+					continue
+				}
+
+				final[h][w] = image[l][h][w]
+				break
 			}
 		}
+	}
+	return final
+}
+
+// Part 2
+func renderImage(img [][]byte) {
+	for i := range img {
+		for j := range img[i] {
+			if img[i][j] == '0' {
+				fmt.Print(" ")
+			} else {
+				fmt.Printf("%s", string(img[i][j]))
+			}
+		}
+		fmt.Println("")
 	}
 }
 
