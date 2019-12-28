7644028fcb89522b59151f164679443e5aa779b4
Aos <aosdab@gmail.com>
2019-12-28 17:28:55 -0500

    Factored out part one and two based on command line flags

--- 2019/day12/main.go
+++ 2019/day12/main.go
@@ -2,11 +2,12 @@
 package main
 
 import (
-	h "aoc/helpers"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"strings"
+
+	h "aoc/helpers"
 )
 
 type axes struct {
@@ -24,19 +25,28 @@ func (m moon) String() string {
 }
 
 var debug = flag.Bool("debug", false, "Print debug info")
+var part = flag.Int("part", 1, "Which part to run?")
+var rounds = flag.Int("rounds", 1000, "How many rounds to run")
 
 func main() {
 	flag.Parse()
-	moons := parseFile("./day12-input.txt")
+	moons := parseFile("./day12-example.txt")
+	start := make([]moon, len(moons))
+	copy(start, moons)
 
+	r := 0
 	// Steps
-	for r := 0; r < 1000; r++ {
+	for {
 		if *debug {
 			fmt.Printf("After %d steps:\n", r)
 			for m := range moons {
 				fmt.Println(moons[m])
 			}
 		}
+		if *part == 1 && r == *rounds {
+			break
+		}
+
 		// 1. Apply gravity
 		//	- Change velocity of every pair of moon by +/- 1 for each axis
 		for i := range moons {
@@ -72,6 +82,15 @@ func main() {
 			moons[i].pos.y += moons[i].vel.y
 			moons[i].pos.z += moons[i].vel.z
 		}
+
+		r++
+
+		if *part == 2 {
+			if compareMoons(start, moons) {
+				fmt.Printf("Part two: %d\n", r)
+				break
+			}
+		}
 	}
 
 	// 3. Potential energy:
@@ -79,13 +98,15 @@ func main() {
 	// 4. Kinetic energy
 	//	- Sum(abs(vel_x), abs(vel_y), abs(vel_z))...)
 	// 5. Total energy = potential * kinetic
-	total := 0
-	for i := range moons {
-		pot := h.Abs(moons[i].pos.x) + h.Abs(moons[i].pos.y) + h.Abs(moons[i].pos.z)
-		kin := h.Abs(moons[i].vel.x) + h.Abs(moons[i].vel.y) + h.Abs(moons[i].vel.z)
-		total += (pot * kin)
+	if *part == 1 {
+		total := 0
+		for i := range moons {
+			pot := h.Abs(moons[i].pos.x) + h.Abs(moons[i].pos.y) + h.Abs(moons[i].pos.z)
+			kin := h.Abs(moons[i].vel.x) + h.Abs(moons[i].vel.y) + h.Abs(moons[i].vel.z)
+			total += (pot * kin)
+		}
+		fmt.Println("Part one:", total)
 	}
-	fmt.Println("Part one:", total)
 }
 
 func parseFile(f string) []moon {
@@ -107,4 +128,29 @@ func parseFile(f string) []moon {
 		}
 	}
 	return moons
+}
+
+// Part 2
+func compareMoons(start, current []moon) bool {
+	for i := range current {
+		if start[i].pos.x != current[i].pos.x {
+			return false
+		}
+		if start[i].pos.y != current[i].pos.y {
+			return false
+		}
+		if start[i].pos.z != current[i].pos.z {
+			return false
+		}
+		if start[i].vel.x != current[i].vel.x {
+			return false
+		}
+		if start[i].vel.y != current[i].vel.y {
+			return false
+		}
+		if start[i].vel.z != current[i].vel.z {
+			return false
+		}
+	}
+	return true
 }
