f2386c5655ed3ced2da66548e5613b5e06d984d5
Aos <aosdab@gmail.com>
2019-12-30 00:13:47 -0500

    Finished day 13 part 2

--- 2019/day13/main.go
+++ 2019/day13/main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"aoc/helpers/opcodes"
+	"fmt"
 )
 
 func main() {
 	oc := opcodes.ReadOpcodesFromFile("./day13-input.txt")
 
-	// fmt.Println("Part one:", PartOne(oc))
-	PartTwo(oc)
+	fmt.Println("Part one:", PartOne(oc))
+	fmt.Println("Part two:", PartTwo(oc))
 }
--- 2019/day13/part2.go
+++ 2019/day13/part2.go
@@ -3,56 +3,66 @@ package main
 
 import (
 	"aoc/helpers/opcodes"
-	"fmt"
 	"image"
 )
 
 // PartTwo ...
 func PartTwo(oc []int) int {
 	oc[0] = 2
-
 	in := make(chan int, 1)
 	out, _ := opcodes.OpcodeVM(oc, in)
 
-	ball := image.Point{}
-	paddle := image.Point{}
+	ball := image.Point{X: -1, Y: -1}
+	paddle := image.Point{X: -1, Y: -1}
+	score := 0
+
 	num := 0
 	var insts [3]int
-	score := 0
-	for {
-		select {
-		case d := <-out:
-			insts[num] = d
-			num = (num + 1) % 3
+	for d := range out {
+		insts[num] = d
+		num = (num + 1) % 3
 
-			if num == 0 {
-				switch {
-				case insts[0] == -1 && insts[1] == 0:
-					score = insts[2]
-					fmt.Println("Score:", score)
+		if num == 0 {
+			x, y, obj := insts[0], insts[1], insts[2]
+			switch {
+			case x == -1 && y == 0:
+				score = obj
+			// Ball
+			case obj == 4:
+				// Initialize location
+				if ball.X == -1 && ball.Y == -1 {
+					ball.X = x
+					ball.Y = y
+					in <- 0
+				} else {
+					dir := direction(ball.X, x)
+					ball.X = x
+					ball.Y = y
 
-				// Ball
-				case insts[2] == 4:
-					ball.X = insts[0]
-					ball.Y = insts[1]
-
-				// Paddle
-				case insts[2] == 3:
-					paddle.X = insts[0]
-					paddle.Y = insts[1]
+					if paddle.X >= ball.X && dir == 1 {
+						in <- 0
+					} else if paddle.X <= ball.X && dir == -1 {
+						in <- 0
+					} else {
+						in <- dir
+					}
 				}
-			}
-		default:
-			//if ball.X > paddle.X {
-			//	in <- 1 // Move paddle right
-			//} else if ball.X < paddle.X {
-			//	in <- -1 // Left
-			//} else {
-			//	in <- 0 // Stay neutral
-			//}
 
-			fmt.Println("Paddle:", paddle)
-			fmt.Println("Ball:", ball)
+			// Paddle
+			case obj == 3:
+				paddle.X = x
+				paddle.Y = y
+			}
 		}
 	}
+	return score
+}
+
+func direction(x0, x1 int) int {
+	if x0 > x1 {
+		return -1
+	} else if x0 < x1 {
+		return 1
+	}
+	return 0
 }
