038b054b73f702f426f683663a2d9d5a07d40988
Aos Dabbagh <aosdab@gmail.com>
2020-12-11 00:23:54 -0500

    start day3

--- 2020/day03/src/main.rs
+++ 2020/day03/src/main.rs
@@ -1,3 +1,28 @@
+use std::fs;
+
+#[derive(Debug)]
+struct Point {
+    x: usize,
+    y: usize,
+    is_tree: bool,
+}
+
+// (x, y)
+struct Slope(usize, usize);
+
 fn main() {
-    println!("Hello, world!");
+    let grid: Vec<Vec<Point>> = fs::read_to_string("example.txt").unwrap()
+        .lines()
+        .enumerate()
+        .map(|(y, line)| parse_line(y, line))
+        .collect();
+
+    let width = grid[0].len();
+}
+
+fn parse_line(y: usize, line: &str) -> Vec<Point> {
+    line
+        .char_indices()
+        .map(|(i, c)| Point { x: i, y, is_tree: c == '#' })
+        .collect()
 }
