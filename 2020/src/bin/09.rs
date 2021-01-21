d5c0ba40b1abe476a4fb8e78bfaeadde07956c77
Aos Dabbagh <aosdab@gmail.com>
2021-01-21 06:27:56 -0500

    finished day 9

--- 2020/src/bin/09.rs
+++ 2020/src/bin/09.rs
@@ -4,11 +4,13 @@ fn main() -> io::Result<()> {
     let file = fs::read_to_string("in/day09_input.txt")?;
     let input: Vec<usize> = file.trim().lines().map(|l| l.parse().unwrap()).collect();
 
-    println!("Part one: {}", solve(&input, 25));
+    let invalid_num = find_invalid(&input, 25);
+    println!("Part one: {}", invalid_num);
+    println!("Part two: {}", find_weakness(&input, invalid_num));
     Ok(())
 }
 
-fn solve(nums: &Vec<usize>, pre_len: usize) -> usize {
+fn find_invalid(nums: &Vec<usize>, pre_len: usize) -> usize {
     let (mut head, mut tail) = (0, pre_len - 1);
     let mut next = tail + 1;
 
@@ -37,7 +39,24 @@ fn solve(nums: &Vec<usize>, pre_len: usize) -> usize {
         tail += 1;
         next += 1;
     }
+    0
+}
 
+fn find_weakness(nums: &Vec<usize>, invalid: usize) -> usize {
+    for i in 0..nums.len()-1 {
+        let mut current_sum = nums[i];
+
+        for j in i+1..nums.len() {
+            current_sum += nums[j];
+
+            if current_sum == invalid {
+                let mut range = vec![0; j-i+1];
+                range.clone_from_slice(&nums[i..=j]);
+                range.sort();
+                return range.first().unwrap() + range.last().unwrap();
+            }
+        }
+    };
     0
 }
 
@@ -69,10 +88,13 @@ mod tests {
     #[test]
     fn text_example_1() {
         let input: Vec<usize> = EX.trim().lines().map(|l| l.parse().unwrap()).collect();
-        assert_eq!(solve(&input, 5), 127);
+        assert_eq!(find_invalid(&input, 5), 127);
     }
 
+    #[test]
     fn text_example_2() {
         let input: Vec<usize> = EX.trim().lines().map(|l| l.parse().unwrap()).collect();
+        let invalid = find_invalid(&input, 5);
+        assert_eq!(find_weakness(&input, invalid), 62);
     }
 }
