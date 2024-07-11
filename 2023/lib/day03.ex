7d798446dd9970cadb017337abde129b207d4903
Aos Dabbagh <25783780+aos@users.noreply.github.com>
2024-07-11 10:36:44 -0400

    3.2

--- 2023/lib/day03.ex
+++ 2023/lib/day03.ex
@@ -2,9 +2,9 @@ defmodule Aoc2023.Day03 do
   alias Aoc2023
 
   def part1(input) do
-    %{numbers: numbers, symbols: symbols} = parse_input(input) 
+    %{numbers: numbers, symbols: symbols} = parse_input(input)
     symbol_positions = Map.keys(symbols)
-    
+
     numbers
     |> Enum.filter(&next_to_symbol?(&1, symbol_positions))
     |> Enum.map(fn {_pos, %{length: _, number: num}} -> num end)
@@ -12,10 +12,15 @@ defmodule Aoc2023.Day03 do
   end
 
   def part2(input) do
-    %{numbers: _numbers, symbols: symbols} = parse_input(input) 
+    %{numbers: numbers, symbols: symbols} = parse_input(input)
+    number_positions = numbers_covered_positions(numbers)
 
     symbols
-    |> Enum.filter(&is_gear?/1)
+    |> Map.filter(fn {_p, %{symbol: s}} -> s === "*" end)
+    |> Enum.map(&elem(&1, 0))
+    |> Enum.map(&adjacent_numbers(&1, number_positions))
+    |> Enum.filter(fn nums -> length(nums) == 2 end)
+    |> Enum.reduce(0, fn [a, b], acc -> acc + a * b end)
   end
 
   def example1 do
@@ -56,8 +61,8 @@ defmodule Aoc2023.Day03 do
   end
 
   def read_symbols(row, row_no, acc) do
-    symbols = Regex.scan(~r/[^[.0-9]+/, row, return: :binary) 
-    indices = Regex.scan(~r/[^[.0-9]+/, row, return: :index) 
+    symbols = Regex.scan(~r/[^[.0-9]+/, row, return: :binary)
+    indices = Regex.scan(~r/[^[.0-9]+/, row, return: :index)
 
     symbols
     |> Enum.zip(indices)
@@ -72,22 +77,38 @@ defmodule Aoc2023.Day03 do
     |> Enum.any?(&Enum.member?(symbol_positions, &1))
   end
 
-  def calculate_surrounding_positions({row, col}, length) do
+  def calculate_surrounding_positions({row, col}, length \\ 1) do
     [{row, col - 1}, {row, col + length}] ++
       row_positions(row - 1, col, length) ++ row_positions(row + 1, col, length)
   end
-  
+
   def row_positions(row, col, length) do
     Enum.map((col - 1)..(col + length), &{row, &1})
   end
 
-  def is_gear?({_position, %{symbol: symbol}}) do
-    symbol === "*"
+  def numbers_covered_positions(numbers) do
+    numbers
+    |> Enum.reduce([], fn {{row, col}, %{number: number, length: length}}, acc ->
+      positions = Enum.map(col..(col + length - 1), fn col -> {row, col} end) |> MapSet.new()
+      [{number, positions} | acc]
+    end)
+  end
+
+  def adjacent_numbers(position, numbers) do
+    surrounding_positions =
+      calculate_surrounding_positions(position)
+      |> MapSet.new()
+
+    numbers
+    |> Enum.reject(fn {_number, positions} ->
+      MapSet.disjoint?(positions, surrounding_positions)
+    end)
+    |> Enum.map(&elem(&1, 0))
   end
 
   def part1_ex_verify, do: example1() |> part1()
   def part2_ex_verify, do: example1() |> part2()
 
   def part1_verify, do: Aoc2023.input("03") |> part1()
-  def part1_verify, do: Aoc2023.input("03") |> part2()
+  def part2_verify, do: Aoc2023.input("03") |> part2()
 end
