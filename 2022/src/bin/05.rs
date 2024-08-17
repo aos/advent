e8e914ffacb298ec6f46c1c2d9dd797f13f01b4c
Aos Dabbagh <25783780+aos@users.noreply.github.com>
2024-08-17 13:38:06 -0400

    2022.5

--- 2022/src/bin/05.rs
+++ 2022/src/bin/05.rs
@@ -1,39 +1,81 @@
+use regex::Regex;
+use std::cell::LazyCell;
 use std::error::Error;
 use std::fs;
-use std::cell::LazyCell;
 
-const EX: &str = r#"
-    [D]
-[N] [C]
-[Z] [M] [P]
- 1   2   3
+#[derive(Debug, Clone)]
+struct Cr(String, usize);
 
-move 1 from 2 to 1
-move 3 from 1 to 3
-move 2 from 2 to 1
-move 1 from 1 to 2
-"#;
-
+type Instructions = Vec<(usize, usize, usize)>;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let f = fs::read_to_string("in/day05_in.txt")?;
-    // println!("{:?}", parse(EX.trim_start_matches("\n")).0);
-    println!("{:#?}", parse(&f));
+    let f = fs::read_to_string("./in/day05_in.txt")?;
+    println!("part 1: {}", part1(&f));
+    println!("part 2: {}", part2(&f));
     Ok(())
 }
 
-#[derive(Debug)]
-struct Cr(String, usize);
+fn part1(inp: &str) -> String {
+    let (mut stacks, instructions) = parse(inp);
+    for inst in instructions {
+        for _ in 0..inst.0 {
+            let taken = stacks[inst.1].pop().unwrap();
+            stacks[inst.2].push(taken);
+        }
+    }
 
-fn parse(inp: &str) -> (Vec<Vec<Cr>>, (usize, usize, usize)) {
-    let (starting_stacks, procedure) = inp.split_once("\n\n").expect("should work");
-    let stacks = parse_stacks(starting_stacks);
-
-    (stacks, (1, 1, 1))
+    stacks
+        .iter()
+        .map(|s| s.last().unwrap().0.clone())
+        .collect::<String>()
 }
 
-fn parse_instructions(inp: &str) -> usize {
-    3
+fn part2(inp: &str) -> String {
+    let (mut stacks, instructions) = parse(inp);
+    for inst in instructions {
+        let len = stacks[inst.1].len();
+        let mut taken = stacks[inst.1].split_off(len - inst.0);
+        stacks[inst.2].append(&mut taken);
+    }
+
+    stacks
+        .iter()
+        .map(|s| s.last().unwrap().0.clone())
+        .collect::<String>()
+}
+
+fn parse(inp: &str) -> (Vec<Vec<Cr>>, Instructions) {
+    let (starting_stacks, instructions) = inp.split_once("\n\n").expect("should work");
+    let stacks = parse_stacks(starting_stacks);
+    let instructions = parse_instructions(instructions);
+
+    (stacks, instructions)
+}
+
+fn parse_instructions(inp: &str) -> Instructions {
+    let re: LazyCell<Regex> =
+        LazyCell::new(|| Regex::new(r"move (\d+) from (\d+) to (\d+)").unwrap());
+
+    inp.lines()
+        .map(|line| {
+            let caps = re.captures(line).unwrap();
+            let num = caps
+                .get(1)
+                .map(|m| m.as_str().parse::<usize>().unwrap())
+                .unwrap();
+            let from = caps
+                .get(2)
+                .map(|m| m.as_str().parse::<usize>().unwrap())
+                .unwrap();
+            let to = caps
+                .get(3)
+                .map(|m| m.as_str().parse::<usize>().unwrap())
+                .unwrap();
+
+            // zero index, subtract 1
+            (num, from - 1, to - 1)
+        })
+        .collect()
 }
 
 fn parse_stacks(inp: &str) -> Vec<Vec<Cr>> {
@@ -62,18 +104,51 @@ fn parse_stacks(inp: &str) -> Vec<Vec<Cr>> {
 
     crs.pop(); // remove the stack labels
     crs.reverse(); // bottom to top
-    crs
+    arrange(crs)
+}
+
+fn arrange(stacks: Vec<Vec<Cr>>) -> Vec<Vec<Cr>> {
+    let max_len = stacks
+        .iter()
+        .flat_map(|v| v.iter().map(|cr| cr.1))
+        .max()
+        .unwrap();
+    let mut result: Vec<Vec<Cr>> = vec![vec![]; max_len + 1];
+
+    for row in stacks.into_iter() {
+        for cr in row.into_iter() {
+            result[cr.1].push(cr);
+        }
+    }
+
+    result
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    const EX: &str = r#"
+    [D]
+[N] [C]
+[Z] [M] [P]
+ 1   2   3
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2
+"#;
+
     #[test]
     fn example_1() {
-        println!("{:?}", EX.trim_start_matches("\n").split_once("\n\n"));
+        let s = part1(EX.trim_start_matches("\n"));
+        assert_eq!(&s, "CMZ");
     }
 
     #[test]
-    fn example_2() {}
+    fn example_2() {
+        let s = part2(EX.trim_start_matches("\n"));
+        assert_eq!(&s, "MCD");
+    }
 }
