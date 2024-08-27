040246ce47d800d9cc201d94c6270fda9ee5e415
Aos Dabbagh <25783780+aos@users.noreply.github.com>
2024-08-27 01:55:46 -0400

    wip

--- 2022/src/bin/08.rs
+++ 2022/src/bin/08.rs
@@ -3,7 +3,10 @@ use std::io;
 fn main() -> io::Result<()> {
     let z = parse_input(EX);
 
-    println!("should be 0 -> {}", z.pos_at(4, 4));
+    let max_row = z.max_each_row();
+    let max_col = z.max_each_col();
+
+    println!("row: {:?}, col: {:?}", max_row, max_col);
 
     Ok(())
 }
@@ -40,10 +43,45 @@ impl Grid {
         self.pos[self.dim.1 * row + col]
     }
 
+    fn pos_2d(&self, idx: usize) -> (usize, usize) {
+        assert!(idx < self.pos.len());
+        (idx / self.dim.1, idx % self.dim.1)
+    }
+
     fn max_each_row(&self) -> Vec<u32> {
+        self.pos
+            .chunks(self.dim.1)
+            .map(|chunk| *chunk.iter().max().unwrap_or(&0))
+            .collect()
     }
 
     fn max_each_col(&self) -> Vec<u32> {
+        (0..self.dim.1)
+            .map(|col| {
+                self.pos
+                    .iter()
+                    .skip(col)
+                    .step_by(self.dim.1)
+                    .max()
+                    .unwrap_or(&0)
+                    .clone()
+            })
+            .collect::<Vec<_>>()
+    }
+
+    fn visible_trees(&self) -> usize {
+        let mut total = (self.dim.0 * 2) + (self.dim.1 * 2) - 4;
+        let max_row = self.max_each_row();
+        let max_col = self.max_each_col();
+
+        self.pos.iter().skip(self.dim.1).enumerate().fold(total, |acc, (idx, x)| {
+            let (row, col) = self.pos_2d(idx);
+            // Skip first and last columns
+            if col == 0 || col == self.dim.1 {
+                acc
+            } else {
+            }
+        })
     }
 }
 
