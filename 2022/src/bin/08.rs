a1bfacb13aef2bd5b512fea3d153166742b978e8
Aos Dabbagh <25783780+aos@users.noreply.github.com>
2024-09-13 10:54:55 -0400

    2022.08.1

--- 2022/src/bin/08.rs
+++ 2022/src/bin/08.rs
@@ -1,14 +1,21 @@
+use std::fs;
 use std::io;
 
 fn main() -> io::Result<()> {
-    let z = parse_input(EX);
+    let f = fs::read_to_string("./in/day08_in.txt")?;
+    let grid = parse_input(&f);
 
-    let max_row = z.max_each_row();
-    let max_col = z.max_each_col();
-
-    println!("row: {:?}, col: {:?}", max_row, max_col);
+    println!("part 1: {}", part1(&grid));
 
     Ok(())
+}
+
+fn part1(grid: &Grid) -> usize {
+    grid.pos
+        .iter()
+        .enumerate()
+        .filter(|(idx, _)| grid.check_edges(*idx))
+        .count()
 }
 
 fn parse_input(inp: &str) -> Grid {
@@ -35,8 +42,9 @@ struct Grid {
     dim: (usize, usize),
 }
 
+#[allow(dead_code)]
 impl Grid {
-    fn pos_at(&self, row: usize, col: usize) -> u32 {
+    fn value_at(&self, row: usize, col: usize) -> u32 {
         assert!(row < self.dim.0);
         assert!(col < self.dim.1);
 
@@ -48,7 +56,40 @@ impl Grid {
         (idx / self.dim.1, idx % self.dim.1)
     }
 
-    // position of max item on each row
+    fn check_edges(&self, current_pos: usize) -> bool {
+        let (row_two_d, col_two_d) = self.pos_2d(current_pos);
+        if row_two_d == 0
+            || col_two_d == 0
+            || row_two_d == (self.dim.0 - 1)
+            || col_two_d == (self.dim.1 - 1)
+        {
+            return true;
+        }
+
+        let range_row_start = row_two_d * self.dim.1;
+        let range_row_end = (row_two_d * self.dim.1) + self.dim.1 - 1;
+        let range_col_end = (self.dim.0 - 1) * self.dim.1 + col_two_d;
+
+        let me = self.pos[current_pos];
+
+        let left = (range_row_start..current_pos)
+            .map(|p| self.pos[p])
+            .all(|v| me > v);
+        let right = (current_pos + 1..=range_row_end)
+            .map(|p| self.pos[p])
+            .all(|v| me > v);
+        let up = (col_two_d..current_pos)
+            .step_by(self.dim.1)
+            .map(|p| self.pos[p])
+            .all(|v| me > v);
+        let down = (current_pos + self.dim.1..=range_col_end)
+            .step_by(self.dim.1)
+            .map(|p| self.pos[p])
+            .all(|v| me > v);
+
+        left || right || up || down
+    }
+
     fn max_each_row(&self) -> Vec<usize> {
         self.pos
             .chunks(self.dim.1)
@@ -57,8 +98,10 @@ impl Grid {
                 chunk
                     .iter()
                     .enumerate()
-                    .max_by_key(|(_idx, x)| **x).unwrap()
-                    .0 + (idx * self.dim.1)
+                    .max_by_key(|(_idx, x)| **x)
+                    .unwrap()
+                    .0
+                    + (idx * self.dim.1)
             })
             .collect()
     }
@@ -71,32 +114,35 @@ impl Grid {
                     .skip(col)
                     .step_by(self.dim.1)
                     .enumerate()
-                    .max_by_key(|(_idx, x)| **x).unwrap()
+                    .max_by_key(|(_idx, x)| **x)
+                    .unwrap()
                     .clone()
-                    .0 * self.dim.1 + col
+                    .0
+                    * self.dim.1
+                    + col
             })
             .collect()
     }
-
-    fn _visible_trees(&self) -> usize {
-        let mut total = (self.dim.0 * 2) + (self.dim.1 * 2) - 4;
-        let max_row = self.max_each_row();
-        let max_col = self.max_each_col();
-
-        self.pos.iter().skip(self.dim.1).enumerate().fold(total, |acc, (idx, x)| {
-            let (row, col) = self.pos_2d(idx);
-            // Skip first and last columns
-            if col == 0 || col == self.dim.1 {
-                acc
-            } else {
-                acc
-            }
-        })
-    }
 }
 
-static EX: &str = "30373
+#[cfg(test)]
+mod tests {
+    use super::*;
+    static EX: &str = "30373
 25512
 65332
 33549
 35390";
+
+    #[test]
+    fn example_1() {
+        let grid = parse_input(EX);
+        let total = grid
+            .pos
+            .iter()
+            .enumerate()
+            .filter(|(idx, _)| grid.check_edges(*idx))
+            .count();
+        assert_eq!(total, 21);
+    }
+}
